9b24968236f924d4b1a4ac6bec46afa22992dd8c
Ligerwolfe <erunyan@me.com>
2025-02-02 08:34:17 -0500

    Speed Test Update

--- speedAnalysis/speed_analysis.js
+++ speedAnalysis/speed_analysis.js
@@ -32,6 +32,9 @@ function endTest() {
 
     var wpm = 0; // Default value
 
+    var typedTextLength = userTypedText.length;
+    
+
     if (timeElapsed !== 0 && !isNaN(typedWords)) {
         wpm = Math.round((typedWords / timeElapsed) * 60);
     }
@@ -39,6 +42,7 @@ function endTest() {
     // Display the results
     var outputDiv = document.getElementById("output");
     outputDiv.innerHTML = "<h2>Typing Test Results:</h2>" +
+        "<p>Total Length: " + typedTextLength + "</p>" +
         "<p>Words Typed: " + typedWords + "</p>" +
         "<p>Time Elapsed: " + timeElapsed.toFixed(2) + " seconds</p>" +
         "<p>Words Per Minute (WPM): " + wpm + "</p>";

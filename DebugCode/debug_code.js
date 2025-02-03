207ea46385df69c599f4260eb7335ba098e82a58
Ligerwolfe <erunyan@me.com>
2025-02-02 19:51:59 -0500

    Debug Final

--- DebugCode/debug_code.js
+++ DebugCode/debug_code.js
@@ -5,7 +5,7 @@ function performOperation(){
     // Check if inputs are valid number
     if(!isNaN(num1) && !isNaN(num2)){
         // Preform the operation
-            let result = multiply(num1, num2);
+            let result = divide(num1, num2);
 
             // Display result
             displayResult(result);
@@ -14,12 +14,12 @@ function performOperation(){
     }
 }
 
-function multiply(a, b){
+function divide(a, b){
     // Introduce a debugger statement to pause execution
     debugger;
 
     // Multiply the numbers
-    return a * b;
+    return a / b;
 }
 
 function displayResult(result){
